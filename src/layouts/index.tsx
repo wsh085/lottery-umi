e247f39fe9ca990e1d0f47746156688e3f6e536c
weishh <763569906@qq.com>
2024-10-08 16:12:14 +0800

    feat: 🎸 增加历史数据查询方法

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,34 +1,38 @@
 import { Link, Outlet } from "umi";
+import { ConfigProvider as AntConfigProvider } from "antd";
 import "styles/index.less";
+import zhCN from "antd/locale/zh_CN";
 import styles from "./index.less";
 
 export default function Layout() {
   return (
-    <div className={styles.navs}>
-      <ul>
-        <li>
-          <Link to="/">首页</Link>
-        </li>
-        <li>
-          <Link to="/hongqiu">红球_走势统计</Link>
-        </li>
-        <li>
-          <Link to="/number">数字_统计</Link>
-        </li>
-        <li>
-          <Link to="/preshahao">前区杀号</Link>
-        </li>
-        <li>
-          <Link to="/piantaifenxi">偏态分析</Link>
-        </li>
-        <li>
-          <Link to="/lishichaxun">历史查询</Link>
-        </li>
-        {/* <li>
+    <AntConfigProvider locale={zhCN}>
+      <div className={styles.navs}>
+        <ul>
+          <li>
+            <Link to="/">首页</Link>
+          </li>
+          <li>
+            <Link to="/hongqiu">红球_走势统计</Link>
+          </li>
+          <li>
+            <Link to="/number">数字_统计</Link>
+          </li>
+          <li>
+            <Link to="/preshahao">前区杀号</Link>
+          </li>
+          <li>
+            <Link to="/piantaifenxi">偏态分析</Link>
+          </li>
+          <li>
+            <Link to="/lishichaxun">历史查询</Link>
+          </li>
+          {/* <li>
           <Link to="/tensorflow">学习预测</Link>
         </li> */}
-      </ul>
-      <Outlet />
-    </div>
+        </ul>
+        <Outlet />
+      </div>
+    </AntConfigProvider>
   );
 }
--- src/pages/lishichaxun.tsx
+++ src/pages/lishichaxun.tsx
@@ -1,9 +1,10 @@
 import { useMemo, useState } from "react";
-import { Button, Col, Form, Input, Row, Table } from "antd";
+import { Alert, Button, Col, Form, Input, Row, Space, Table } from "antd";
 import { getAllHistoryData } from "@/utils";
 import { FORM_ITEM_LAYOUT } from "@/utils/constants";
 import useObjectState from "@/hooks/useObjectState";
 import { uniq } from "lodash-es";
+import LiShiChaXunModal from "./components/LiShiChaXunModal";
 
 /**
  * 历史数据查询
@@ -18,6 +19,10 @@ const LiShiChaXun = () => {
   }>({
     red: undefined,
     blue: undefined,
+  });
+
+  const [modalParams, updateModalParams] = useObjectState({
+    open: false,
   });
 
   const red: string[] = params.red?.split(" ") || []; // 红球
@@ -46,8 +51,14 @@ const LiShiChaXun = () => {
   // 计算重复出现的号码
   const chongfuNumber = useMemo(() => {
     if (dataSource.length) {
+      // 重复出现的红球结果
       const redResult: string[] = [];
+      // 重复出现的蓝球结果
       const blueResult: string[] = [];
+      // 重复出现的红球结果的 出现次数 统计
+      const redCount: any = {};
+      // 重复出现的蓝球结果的 出现次数 统计
+      const blueCount: any = {};
       const allRedNumber: string[] = [];
       const allBlueNumber: string[] = [];
 
@@ -60,6 +71,7 @@ const LiShiChaXun = () => {
         redqiu.forEach((key: string) => {
           if (allRedNumber.includes(key)) {
             redResult.push(key);
+            redCount[key] = (redCount[key] || 1) + 1;
           } else {
             allRedNumber.push(key);
           }
@@ -68,6 +80,7 @@ const LiShiChaXun = () => {
         blueqiu.forEach((key: string) => {
           if (allBlueNumber.includes(key)) {
             blueResult.push(key);
+            blueCount[key] = (blueCount[key] || 1) + 1;
           } else {
             allBlueNumber.push(key);
           }
@@ -76,7 +89,9 @@ const LiShiChaXun = () => {
 
       return {
         red: uniq(redResult.sort((a, b) => Number(a) - Number(b))),
+        redCount,
         blue: uniq(blueResult.sort((a, b) => Number(a) - Number(b))),
+        blueCount,
       };
     }
     return {
@@ -119,10 +134,38 @@ const LiShiChaXun = () => {
             </Form.Item>
           </Col>
           <Col span={8}>
-            <Button onClick={() => handleDataSource()}>查询</Button>
+            <Space>
+              <Button className="w-1-1" onClick={() => handleDataSource()}>
+                查询
+              </Button>
+              <Button
+                className="w-1-1"
+                onClick={() => updateModalParams({ open: true })}
+              >
+                方法验证
+              </Button>
+            </Space>
           </Col>
         </Row>
       </Form>
+
+      <Alert
+        type="info"
+        className="mt-24"
+        message="查询方法"
+        closable
+        description={
+          <div>
+            <div>1、取红球的前三个号码 进行查询</div>
+            <div>2、取红球的中间三个号码 进行查询</div>
+            <div>3、取红球的后2个号码 + 蓝球第1个号码 进行查询</div>
+            <div>
+              4、取红球的第3、第4号码 + 蓝球第1个号码/第2个号码/全部号码
+              进行查询
+            </div>
+          </div>
+        }
+      />
 
       <Table
         className="mt-24"
@@ -170,7 +213,22 @@ const LiShiChaXun = () => {
         chongfuNumber.red?.length ? (
           <div className="mt-24">
             <div>红球重复出现的号码：</div>
-            <div>{chongfuNumber.red?.join("  ")}</div>
+            <div>
+              {chongfuNumber.red?.map((number) => {
+                if (chongfuNumber.redCount[number] > 2) {
+                  return (
+                    <span className="mr-8">
+                      {number}(
+                      <span className="text-blue">
+                        {chongfuNumber.redCount[number]}
+                      </span>
+                      )
+                    </span>
+                  );
+                }
+                return <span className="mr-8">{number}</span>;
+              })}
+            </div>
           </div>
         ) : null
       }
@@ -179,10 +237,31 @@ const LiShiChaXun = () => {
         chongfuNumber.blue?.length ? (
           <div className="mt-24">
             <div>蓝球重复出现的号码：</div>
-            <div>{chongfuNumber.blue?.join("  ")}</div>
+            <div>
+              {chongfuNumber.blue?.map((number) => {
+                if (chongfuNumber.blueCount[number] > 2) {
+                  return (
+                    <span className="mr-8">
+                      {number}(
+                      <span className="text-blue">
+                        {chongfuNumber.blueCount[number]}
+                      </span>
+                      )
+                    </span>
+                  );
+                }
+                return <span className="mr-8">{number}</span>;
+              })}
+            </div>
           </div>
         ) : null
       }
+      <LiShiChaXunModal
+        open={modalParams.open}
+        // handleShaHao={handleShaHao}
+        onOk={() => updateModalParams({ open: false })}
+        onCancel={() => updateModalParams({ open: false })}
+      />
     </div>
   );
 };
