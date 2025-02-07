9b4d9cc68d146a48ff08ee060580c32954ac5a9f
weishh <763569906@qq.com>
2025-02-07 19:10:44 +0800

    feat: 🎸 更新数据

--- script/lotteryYuCe/index.js
+++ script/lotteryYuCe/index.js
@@ -1,12 +1,12 @@
 const StatisticalPredictor = require("./statisticalPredictor.js");
-const fs = require("fs").promises;
+const fs = require("fs");
+const path = require("path");
 
 async function main() {
   try {
     // 读取历史数据
-    const historyData = JSON.parse(
-      await fs.readFile("../../data/all_history_data.json", "utf8")
-    );
+    const dataPath = path.join(__dirname, "../../data/all_history_data.json");
+    const historyData = JSON.parse(fs.readFileSync(dataPath, "utf8"));
 
     // 创建预测器实例
     const predictor = new StatisticalPredictor();
